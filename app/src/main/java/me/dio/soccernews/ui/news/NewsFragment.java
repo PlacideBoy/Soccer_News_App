f803fd60eee403426fd42784f4ed53ab6693a38a
JorgeLucas <83237763+PlacideBoy@users.noreply.github.com>
2022-06-30 20:59:15 -0300

    Implementação das funções "Abrir Link" e "Compartilhar"

--- app/src/main/java/me/dio/soccernews/ui/news/NewsFragment.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsFragment.java
@@ -1,6 +1,7 @@
 package me.dio.soccernews.ui.news;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,7 +31,9 @@ public class NewsFragment extends Fragment {
 
         binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext()));
         newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> {
-            binding.rvNews.setAdapter(new NewsAdapter(news));
+            binding.rvNews.setAdapter(new NewsAdapter(news, view -> {
+
+            }));
         });
         return root;
     }
